1df844ee05450aee2e4dad9a216c362e0cf3c4b9
oeyekul <oeyekul@live.com>
2026-09-24 13:52:51 +0800

    Rename site to Luke's Moving Out Sale

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,3 @@
 import './globals.css';
-export const metadata={title:"Yeo’s Moving Out Sale",description:"Moving out sale"};
+export const metadata={title:"Luke’s Moving Out Sale",description:"Moving out sale"};
 export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
