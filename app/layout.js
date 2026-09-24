1e2d0c605b3cabac352f8042d967cde642bd7f25
oeyekul <oeyekul@live.com>
2026-09-24 10:26:04 +0800

    Build moving out sale

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:"Yeo’s Moving Out Sale",description:"Moving out sale"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
